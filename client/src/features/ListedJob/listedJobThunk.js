8209a235dccc3027d64b62c265950578f139f6da
shapnesh singh tiwari <shapnesht@gmail.com>
2024-03-07 16:09:06 +0530

    added corsproxy in url

--- client/src/features/ListedJob/listedJobThunk.js
+++ client/src/features/ListedJob/listedJobThunk.js
@@ -4,7 +4,7 @@ import { checkForUnauthorizedResponse } from '../../utils/axios'
 export const getAllListedJobsThunk = async (_, thunkAPI) => {
   const { searchTerm } = thunkAPI.getState().listedJobs
 
-  let url = `https://api.cuvette.tech/api/v1/externaljobs?search=${searchTerm}`
+  let url = `https://corsproxy.io/?https://api.cuvette.tech/api/v1/externaljobs?search=${searchTerm}`
 
   try {
     const resp = await axios.get(url)
